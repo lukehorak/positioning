2bcd8e7910646c6667aac9b458f24fdac7f88a0f
Luke Horak <lukasbhorak@gmail.com>
2019-05-28 19:15:53 +0000

    initial commit

---
+++ positioning.js
@@ -0,0 +1,20 @@
+function countLetters(phrase){
+  const stripped = phrase.replace(' ', '');
+
+  let results = {}
+
+  for (let c in stripped){
+    if(!results[stripped[c]]){
+      results[stripped[c]] = [c];
+    }
+    else {
+      results[stripped[c]].push(c)
+    }
+  }
+  return results
+}
+
+// Tests
+
+console.log(countLetters('hello there'));
+console.log(countLetters('general kenobi!'));
